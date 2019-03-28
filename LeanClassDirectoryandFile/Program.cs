72ef8a0de80f0aceccc0ff6597ec35c9357c78f5
ivanovip1934 <ivanovip1934@outlook.com>
2019-03-28 21:34:41 +0300

    Full Solution LeanClasses Directory and Files

---
+++ LeanClassDirectoryandFile/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LeanClassDirectoryandFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles(@"c:\windows\", "*.log"); //, SearchOption.AllDirectories);
+            foreach (string  _string in files)
+            {
+                Console.WriteLine("File name: {0}", _string);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
